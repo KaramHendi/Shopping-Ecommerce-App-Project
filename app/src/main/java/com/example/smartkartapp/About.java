bebdb6a7fd15c4b73edd37a1218a0b1ab0df768b
KARAMHENDIZD <karamhendizd10@gmail.com>
2025-03-27 15:09:12 +0200

    2

--- app/src/main/java/com/example/smartkartapp/About.java
+++ app/src/main/java/com/example/smartkartapp/About.java
@@ -11,21 +11,19 @@ import android.widget.Button;
 import java.net.URI;
 
 public class About extends AppCompatActivity {
-    Button fb,tw,qr,ld,gt,ig;
+    Button fb,tw,gt,ig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         fb=findViewById(R.id.fb);
         gt=findViewById(R.id.gt);
-        qr=findViewById(R.id.qr);
         tw=findViewById(R.id.tw);
         ig=findViewById(R.id.ig);
-        ld=findViewById(R.id.ld);
         ig.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/vijayjoshi802/"));
+                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.instagram.com/karam_hendi_/"));
                 startActivity(intent);
             }
         });
@@ -33,7 +31,7 @@ public class About extends AppCompatActivity {
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/profile.php?id=100007797368212"));
+                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.facebook.com/profile.php?id=61568597606386&locale=ar_AR"));
                 startActivity(intent);
             }
         });
@@ -41,33 +39,19 @@ public class About extends AppCompatActivity {
         gt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://github.com/vijayjoshi16"));
+                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://github.com/KaramHendi"));
                 startActivity(intent);
             }
         });
 
-        ld.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://in.linkedin.com/in/vijay-joshi-80221a193"));
-                startActivity(intent);
-            }
-        });
 
         tw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://twitter.com/vijay_joshi16"));
+                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://x.com/karamhendizd"));
                 startActivity(intent);
             }
         });
 
-        qr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://www.quora.com/profile/Vijay-Joshi-96"));
-                startActivity(intent);
-            }
-        });
     }
 }
